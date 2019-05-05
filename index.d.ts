6482a692600dfeb48ac3b67ae9cc4feacb603f34
zhangzhihua <zhangzhihua01@58ganji.com>
2019-05-05 17:11:34 +0800

    d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,12 +4,44 @@ export = WF;
 
 declare namespace WF {
 
+    /**
+     * Before 修饰器
+     * 当 Before 没有函数参数时，当前方法被声明为此 class 的勾子函数，所有 action 触发都会先调用此方法
+     * 当 Before 有函数参数时， 函数参数为当前 action 的勾子函数，调用此 action 都会条用函数参数
+     * 函数返回 false 时，action 不执行
+     * @param fn 勾子函数，此参数单独配置 action 的前置方法
+     */
     export function Before(fn?: Function): Function;
+
+    /**
+     * After 修饰器
+     * 使用说明参考 Before 修饰器
+     * @param fn 勾子函数，此参数单独配置 action 的后置方法
+     */
     export function After(fn?: Function): Function;
+
+    /**
+     * Path 修饰器
+     * Path 修饰 class 时，参数为根路由(参数只能一个)
+     * Path 修饰 method 时，参数为方法路由(参数可有多个)
+     * @param props 路由参数
+     */
     export function Path(...props: string[]): Function;
 
+    /**
+     * Private 修饰器
+     * 将 controller action 方法私有，设置后没有默认路由
+     */
     export const Private: Function;
 
+    /**
+     * controllers 配置路由文件使用
+     */
+    export const controllers: any;
+
+    /**
+     * 访问 method
+     */
     export namespace RequestMethod {
         export const GET: Function;
         export const POST: Function;
@@ -21,6 +53,10 @@ declare namespace WF {
         export const CONNECT: Function;
     }
 
+    /**
+     * Controller 基类，提供一些基本方法
+     * TODO
+     */
     export class Controller {
         ctx: Koa.Context;
         req: Koa.Request;
